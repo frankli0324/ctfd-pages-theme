1e7055b612466db179649f63c1fc36bd62dba072
ddomdel <78736225+ddomdel@users.noreply.github.com>
2022-08-02 18:12:11 +0800

    Included the static files in the commit by removing the static folder in gitignore

---
+++ static/assets/scoreboard.e6890429.js
@@ -0,0 +1,1 @@
+import{m as t,C as a}from"./index.6f926540.js";import{g as e,e as i}from"./index.730a65ab.js";import"./echarts.7de7c874.js";window.Alpine=t;window.CTFd=a;t.data("ScoreboardDetail",()=>({data:null,async init(){this.data=await a.pages.scoreboard.getScoreboardDetail(10);let o=e(a.config.userMode,this.data);i(this.$refs.scoregraph,o)}}));t.start();
---
+++ static/assets/users_list.03c81fbe.js
@@ -0,0 +1,1 @@
+import{C as o,m as d}from"./index.6f926540.js";window.CTFd=o;window.Alpine=d;d.start();
