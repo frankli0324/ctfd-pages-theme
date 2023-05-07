c41d8939ac56ede37368c97d538f45699d6850c4
Frank <frankli0324@hotmail.com>
2023-05-08 01:47:56 +0800

    rebuild static assets

---
+++ static/assets/scoreboard.1116050b.js
@@ -0,0 +1,1 @@
+import{m as t,C as a}from"./index.20fcd98e.js";import{g as e,e as i}from"./index.5f3aaf6c.js";import"./echarts.128204f2.js";window.Alpine=t;window.CTFd=a;t.data("ScoreboardDetail",()=>({data:null,async init(){this.data=await a.pages.scoreboard.getScoreboardDetail(10);let o=e(a.config.userMode,this.data);i(this.$refs.scoregraph,o)}}));t.start();
---
+++ static/assets/teams_list.b7db4c9f.js
@@ -0,0 +1,1 @@
+import{C as o,m as d}from"./index.20fcd98e.js";window.CTFd=o;window.Alpine=d;d.start();
